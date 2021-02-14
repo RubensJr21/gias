a8fb0432ea988a91e164c5c8ee8a323b5f0bf404
RubensJr21 <junior.correa2001@gmail.com>
2021-02-14 02:15:22 -0300

    adicionando o uso de variáveis de ambiente

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,23 @@
-function tempo(request, response){
+async function tempo(request, response){
+    const cep = request.query.cep || process.env.CEP;
+
     const dynamicDate = new Date();
 
+    const endereco = await (await fetch(`https://viacep.com.br/ws/${cep}/json/`)).json();
+
+    delete endereco.ibge
+    delete endereco.gia
+    delete endereco.ddd
+    delete endereco.siafi
+    delete endereco.complemento
+
+    /*endereco.json().then(data => {
+        console.log(data)
+    });*/
+    console.log(request.query)
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        cep_result: endereco
     })
 }
 
